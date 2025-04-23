2fe465c6805107325790ffa3a747df00cd534599
Mike Tay <michael.tay@blueacorn.com>
2025-04-23 10:23:08 -0400

    add .mac-setter-upper scripts to che

---
+++ dot_mac-setter-upper/executable_app-casker.sh
@@ -0,0 +1,49 @@
+set -e
+
+# -- Functions --
+usage() {
+  cat << EOF
+Usage: $(basename $0) [-h] [-a app-list]
+This idempotent macOS Bash script uses Homebrew Cask to install basic dev apps...
+  * Docker Desktop
+  * VS Code
+  * Browsers
+    - Chrome
+    - Firefox
+
+OPTIONS
+  -h           display help
+  -a app-list  list of brew cask apps to install
+               e.g. -a "docker visual-studio-code"
+EOF
+}
+
+# -- Command Line Options --
+# Command line arguments over interactive
+while getopts 'a:h' opt; do
+  case "$opt" in
+
+    a)
+      dev_apps="$OPTARG"
+      ;;
+
+    ?|h)
+    usage
+    exit
+      ;;
+  esac
+done
+shift "$(($OPTIND -1))"
+
+# -- MAIN --
+echo "Homebrew installing apps [${dev_apps[*]}]"
+
+# Homebrew is required
+brew help > /dev/null || \
+  (echo "FAIL [1] - Homebrew must be installed first" ; exit 1)
+
+# Install app (or not)
+for app in ${dev_apps[@]}; do
+  brew list --cask "$app" > /dev/null || \
+    (echo "brew install --cask ${app}"; brew install --cask "$app")
+done
