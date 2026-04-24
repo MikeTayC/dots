88da8182b4ca1198c44f732c27e36a74da211eda
Mike Tay <michael.tay@blueacorn.com>
2026-04-24 18:30:51 -0400

    Add .config/zsh/custom/fuzzy.zsh Add .config/zsh/custom/history-sync.custom.zsh Add .config/zsh/custom/history.custom.zsh

---
+++ private_dot_config/zsh/custom/history-sync.custom.zsh
@@ -0,0 +1,12 @@
+# history-sync plugin requires GNU sed - it keeps erroring on macos version 
+# brew install gnu-sed -> gives us gsed - we'll set the alias ehre so it'll work on 
+# zsh history sync plugin - SEE  custom/plugins/before-plugins
+
+# your zsh_history file location
+ZSH_HISTORY_FILE="${HOME}/.zsh_history"
+# your git project for housing your zsh_history file
+ZSH_HISTORY_PROJ="${HOME}/.zsh_history_backups/drunk-zsh-history"
+# your encrypted zsh_history file location
+ZSH_HISTORY_FILE_ENC="${ZSH_HISTORY_PROJ}/zsh_history"
+# your default message when pushing to $ZSH_HISTORY_PROJ
+GIT_COMMIT_MSG="latest $(date)"
---
+++ private_dot_config/zsh/custom/history.custom.zsh
@@ -0,0 +1,17 @@
+#################
+# Customize the various oh-my-zsh history lib/plugin settings
+#################
+
+# increase SAVEHIST to 50000 - This controls the number of entries from HISTFILE into zsh shell memory
+[ "$SAVEHIST" -lt 50000 ] && SAVEHIST=50000
+
+# history range - usage: history -E -l $1 $2
+alias hsr='history -E -l'
+
+alias backup_history="cp $HOME/.zsh_history $HOME/.zsh_history_backups/zsh_history_$(date +\%Y_\%m_\%d).bak"
+
+
+setopt HIST_REDUCE_BLANKS
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_SAVE_NO_DUPS
+setopt HIST_FIND_NO_DUPS
